b0962dcb5cbc5133de20858e9b2ccf77cd195f16
IngeborgStreef <75255444+IngeborgStreef@users.noreply.github.com>
2021-06-10 12:22:57 +0200

    styled main page

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -3,11 +3,11 @@ import './Intro.css';
 
 function Intro() {
     return (
-        <body className="introPage">
-        <div className='circle'>
-            <h1>IT'S A MATT WORLD</h1>
+        <div className="introPage">
+            <div className='circle'>
+                <h1>IT'S A MATT WORLD</h1>
+            </div>
         </div>
-        </body>
     );
 }
 
