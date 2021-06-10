0652246562a6055d36564268a6922c1e90b24b8a
IngeborgStreef <75255444+IngeborgStreef@users.noreply.github.com>
2021-06-10 13:56:06 +0200

    made page for new account

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import './App.css';
+
 import Intro from './pages/Intro';
 import './pages/Intro.css';
 import Main from './pages/Main';
 import './pages/Main.css';
+import CreateAccount from "./pages/CreateAccount";
+import './pages/CreateAccount.css';
+
 import NavigationBar from "./components/NavigationBar";
 import './components/NavigationBar.css'
 
@@ -11,7 +15,8 @@ function App() {
   return (
     <div className="App">
       {/*<Intro />*/}
-      <Main />
+      {/*<Main />*/}
+      <CreateAccount />
     </div>
   );
 }
---
+++ src/pages/CreateAccount.js
@@ -0,0 +1,76 @@
+import React from "react";
+import matt from '../assets/matt.png';
+
+function CreateAccount() {
+    function onFormSubmit(e) {
+        e.preventDefault();
+        console.log("submitted");
+    }
+
+    return (
+        <main className="mainpagecontainerForm">
+            <aside className="matt">
+                <img src={matt} alt="Matt"/>
+            </aside>
+
+            <form onSubmit={onFormSubmit} className="newUserForm">
+                <h1>Nieuwe gebruiker</h1>
+
+                <legend>
+                    <legend>Informatie ouder</legend>
+
+                    <label htmlFor="parent-name">
+                        Naam ouder:
+                        <input type="text"
+                               name="name"
+                               id="details-parent-name"
+                        />
+                    </label>
+
+                    <label htmlFor="email">
+                        E-mail adres:
+                        <input type="text"
+                               name="e-mail"
+                               id="e-mail"
+                        />
+                    </label>
+
+                    <label htmlFor="password">
+                        Wachtwoord:
+                        <input type="text"
+                               name="password"
+                               id="password"
+                        />
+                    </label>
+
+                </legend>
+
+                <legend>
+                    <legend>Informatie kind</legend>
+                    <label htmlFor="kid-name">
+                        Naam kind:
+                        <input type="text"
+                               name="name"
+                               id="details-kid-name"
+                        />
+                    </label>
+
+                    <label htmlFor="details-age">
+                        Leeftijd kind:
+                        <input type="text"
+                               name="age"
+                               id="details-age"
+                        />
+                    </label>
+                </legend>
+
+                <button type="submit" className="button">
+                    Versturen
+                </button>
+
+            </form>
+        </main>
+    );
+}
+
+export default CreateAccount;
